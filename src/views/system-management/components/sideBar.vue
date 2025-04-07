b096e1b7f48a5abf64ddaf658562569e6a38c0e9
rainbow <rainbowcc.wang@gmail.com>
2025-04-07 11:06:27 +0800

    系统设置页面基本完成

---
+++ src/views/system-management/components/sideBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <el-menu
+    active-text-color="#ffd04b"
+    background-color="#545c64"
+    class="el-menu-vertical-demo"
+    default-active="2"
+    text-color="#fff"
+    @open="handleOpen"
+    @close="handleClose"
+  >
+    <el-menu-item index="1">
+      <el-icon><setting /></el-icon>
+      <span>文件上传</span>
+    </el-menu-item>
+    <el-menu-item index="2">
+      <el-icon><setting /></el-icon>
+      <span>上传记录</span>
+    </el-menu-item>
+  </el-menu>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+</style>
--- src/views/system-management/index.vue
+++ src/views/system-management/index.vue
@@ -1,5 +1,57 @@
 <template>
-  <div class="dashboard-container">
-    <div class="dashboard-text">系统管理</div>
-  </div>
+  <el-container>
+    <el-header>
+      <div class="header-left">
+        <img class="logo-img" src="@/assets/images/logo.png" alt="logo" />
+        <img class="logo-text" src="@/assets/logo-text.png" alt="logo-text" />
+      </div>
+      <div class="header-right">
+        <img
+          class="return-img"
+          @click="handleReturn"
+          src="@/assets/return.png"
+          alt="avatar"
+        />
+      </div>
+    </el-header>
+    <el-container>
+      <el-aside width="200px">
+        <SideBar />
+      </el-aside>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
+
+<script setup>
+import SideBar from './components/sideBar.vue'
+import { useRouter } from 'vue-router'
+const router = useRouter()
+const handleReturn = () => {
+  console.log('返回');
+  router.push('/dashboard');
+}
+</script>
+<style lang="less" scoped>
+.header-left {
+  height: 100%;
+  display: flex;
+  align-items: center;
+  margin-left: 30px;
+  .logo-img {
+    height: 50px;
+  }
+  .logo-text {
+    height: 30px;
+    margin-left: 10px;
+  }
+}
+.header-right {
+  .return-img {
+    height: 30px;
+    margin-right: 10px;
+    cursor: pointer;
+  }
+}
+</style>
+
