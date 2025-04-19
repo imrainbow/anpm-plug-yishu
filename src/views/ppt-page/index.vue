40daa7c7041f81cb18d5325c777e209de0a938ce
rainbow <rainbowcc.wang@gmail.com>
2025-04-19 22:31:32 +0800

    页面回到最初始

--- src/views/ppt-page/index.vue
+++ src/views/ppt-page/index.vue
@@ -6,16 +6,19 @@
 
 <script setup>
 import { onMounted } from 'vue'
+import { useRoute } from 'vue-router'
 import { getFile } from '@/api/ppt-file'
-
+import { ref } from 'vue'
+const route = useRoute()
+const id = route.query.id
 
 const getFileSync = async() =>{
   const res = await getFile(id)
-
+  console.log(res, 'res')
 }
 
 onMounted(() => {
-
+  getFileSync()
 })
 
 
