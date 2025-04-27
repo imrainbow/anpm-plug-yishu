6f5aa5899b6062c08c7e1d3bd06ccd6026925ec8
rainbow <rainbowcc.wang@gmail.com>
2025-04-27 14:36:17 +0800

    法律检索，检查办案loading样式处理完成

--- src/views/check-cases/index.vue
+++ src/views/check-cases/index.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="ai-help-container card-page">
+  <div
+    class="ai-help-container card-page"
+    v-loading="loading"
+    element-loading-text="加载中..."
+  >
     <el-tooltip class="box-item" content="返回" placement="left">
       <img
         class="return-img"
@@ -8,22 +12,29 @@
         @click="handleReturn"
       />
     </el-tooltip>
+
     <iframe
       src="https://law.wkinfo.com.cn/"
       width="100%"
       height="100%"
       frameborder="0"
+      @load="handleIframeLoad"
     ></iframe>
   </div>
 </template>
 
 <script setup>
 import { useRouter } from 'vue-router';
+import { ref } from 'vue';
 
 const router = useRouter();
+const loading = ref(true);
 
 const handleReturn = () => {
   router.back();
+};
+const handleIframeLoad = () => {
+  loading.value = false;
 };
 </script>
 
